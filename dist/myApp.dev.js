859497a1f4113c6be144d506acdcee97c8b14a74
snehasil93 <snehasil2009@gmail.com>
2022-08-08 09:59:07 +0530

    initial

---
+++ dist/myApp.dev.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var express = require('express');
+
+var app = express();
+
+var helmet = require('helmet');
+
+module.exports = app;
+
+var api = require('./server.js');
+
+app.use(express["static"]('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Your app is listening on port ".concat(port));
+});
